40a9a7454e7ef9c9aa2de9a019a3506bfaf73361
tayana-lab <engineering@tayana.in>
2026-06-01 19:35:47 +0530

    Refactor Vite configuration for server options

--- vite.config.ts
+++ vite.config.ts
@@ -4,33 +4,28 @@ import path from 'path';
 import fs from 'fs';
 
 export default defineConfig(({ mode }) => {
-  // Plugins
-  const plugins = [react()];
+  const isLocal = fs.existsSync('/opt/tssgui/tssgui/cert/key.pem');
 
-  // Resolve aliases
-  const aliases = {
-    '@app': path.resolve(__dirname, './src'),
-    '@store': path.resolve(__dirname, './src/store'),
-    '@components': path.resolve(__dirname, './src/components'),
-    '@modules': path.resolve(__dirname, './src/modules'),
-    '@pages': path.resolve(__dirname, './src/pages'),
-  };
-
-  // Server configuration
-  const serverOptions = {
-    port: 3030, // Specify the port for the development server
-    https: {
-    key: fs.readFileSync('/opt/tssgui/tssgui/cert/key.pem'), // Path to your SSL key file
-    cert: fs.readFileSync('/opt/tssgui/tssgui/cert/cert.pem'), // Path to your SSL certificate file
-    },
-    host: '10.0.6.102', // Specify the host for the development server
-  };
   return {
-    mode: mode,
-    plugins: plugins,
+    plugins: [react()],
     resolve: {
-      alias: aliases,
+      alias: {
+        '@app': path.resolve(__dirname, './src'),
+        '@store': path.resolve(__dirname, './src/store'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@modules': path.resolve(__dirname, './src/modules'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+      },
     },
-    server: serverOptions,
+    server: {
+      port: 3030,
+      host: '0.0.0.0',
+      ...(isLocal && {
+        https: {
+          key: fs.readFileSync('/opt/tssgui/tssgui/cert/key.pem'),
+          cert: fs.readFileSync('/opt/tssgui/tssgui/cert/cert.pem'),
+        },
+      }),
+    },
   };
 });
